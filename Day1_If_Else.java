797ae7ff05ef65fe1abb3587cde8dba64d24819f
Cindy Lukman <cindi4381@gmail.com>
2022-02-27 21:52:49 -0800

    perbaikan if else

--- Day1_If_Else.java
+++ Day1_If_Else.java
@@ -15,19 +15,21 @@ public class Day1_If_Else {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
+        
         if (N%2==1){//OOD
-            System.out.println("weird");
+            System.out.println("Weird"); 
+            
         }else{//even
         if(2 <=N && N<=5){
-            System.out.println(" not weird");
+            System.out.println("Not Weird");
+            
         }else if (6 <=N && N<=20){
-            System.out.println("weird");
+            System.out.println("Weird");
+        
         }else if(N>20){
-            System.out.println(" not weird");
+             System.out.println("Not Weird");
         }
         }
         scanner.close();
     }
 }
-
